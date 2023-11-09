f87af0aef67d07e2d20083e7bb1a7730e98f076e
0xab1de <16597753+BlackSenPig@users.noreply.github.com>
2023-11-10 07:10:24 +0800

    feat: i18n eslint fix

---
+++ i18n.ts
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    backend: {
+      loadPath: `/locales/{{lng}}/{{ns}}.json`,
+      // process.env.NODE_ENV !== "prod"
+      //   ? `./locales/{{lng}}/{{ns}}.json`
+      //   : `${window.location.pathname}locales/{{lng}}/{{ns}}.json`,
+    },
+    fallbackLng: "en",
+    //lng: "zh",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+//i18n.changeLanguage();
+export default i18n;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { App } from "@/app";
+import { App } from "@/app/app";
 import { BrowserRouter, useRoutes } from "react-router-dom";
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
